b4d7dfb5fd4e5ef559456ee00bf553bc4e44b833
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-11-05 18:48:09 -0500

    Updated LinkedIn Link

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ function Header() {
         <nav className="navigation-bar">
             <div className="nav-logo">Murilo Pereira</div>
             <ul className="nav-links">
-                <li className="nav-item"><a href="#home">LinkedIn</a></li>
+                <li className="nav-item"><a href="https://www.linkedin.com/in/murilo-pereira-it/" target="_blank">LinkedIn</a></li>  {/* target blank to open new tab*/}
                 <li className="nav-item"><a href="#about">Resume</a></li>
                 <li className="nav-item"><a href="#contact">Contact</a></li>
             </ul>
