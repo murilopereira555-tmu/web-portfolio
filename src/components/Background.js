7270eab9cd8ce8c8cb00b338e68ce4b4278ea3f9
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-11-05 18:45:29 -0500

    Updated some css

--- src/components/Background.js
+++ src/components/Background.js
@@ -8,8 +8,6 @@ const Background = () => {
       <div className="g1"></div>
       <div className="g2"></div>
       <div className="g3"></div>
-      <div className="g4"></div>
-      <div className="g5"></div>
       <div className="interactive"></div>
     </div>
   );
