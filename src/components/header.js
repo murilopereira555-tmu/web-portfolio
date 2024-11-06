98fda1fedcdf04c80a3429c97399c56f0e97f9e0
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-11-05 20:22:15 -0500

    Fixed resume link

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ function Header() {
             <div className="nav-logo">Murilo Pereira</div>
             <ul className="nav-links">
                 <li className="nav-item"><a href="https://www.linkedin.com/in/murilo-pereira-it/" target="_blank">LinkedIn</a></li>  {/* target blank to open new tab*/}
-                <li className="nav-item"><a href="/MuriloPereira_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
+                <li className="nav-item"><a href="https://drive.google.com/file/d/1vpeqR-ronYRkARtulJB0cCf876Ao_0ml/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
                 <li className="nav-item"><a href="#contact">Contact</a></li>
             </ul>
         </nav>
