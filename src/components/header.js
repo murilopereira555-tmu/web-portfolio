fea5ba5d1050fcc34d52f7eb350180a5d80212f2
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-11-05 20:09:59 -0500

    added resume

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ function Header() {
             <div className="nav-logo">Murilo Pereira</div>
             <ul className="nav-links">
                 <li className="nav-item"><a href="https://www.linkedin.com/in/murilo-pereira-it/" target="_blank">LinkedIn</a></li>  {/* target blank to open new tab*/}
-                <li className="nav-item"><a href="#about">Resume</a></li>
+                <li className="nav-item"><a href="/MuriloPereira_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
                 <li className="nav-item"><a href="#contact">Contact</a></li>
             </ul>
         </nav>
