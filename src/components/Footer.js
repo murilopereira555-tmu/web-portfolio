6a2cffc3d008ccfa1115b80e31a550e687660c49
murilopereira555-tmu <murilo.pereira@torontomu.ca>
2024-11-28 17:20:04 -0500

    Fixed broken links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,19 +35,19 @@ function Footer() {
                     description={"Submission for Lab8"}
                     expandedtext={"Here are my images, alt text, and audio player"}  // Pass expanded text if any
                     images={[{ 
-                        url: "https://cdn.discordapp.com/attachments/1108482530417594461/1306057478772887652/IMG_0477.jpg?ex=67486610&is=67471490&hm=023a688575e672855acab1fe7fc2f2f38674b28519b56b32e1f5ac7b05b632eb&", 
+                        url: "https://cdn.discordapp.com/attachments/1108482530417594461/1306057478772887652/IMG_0477.jpg?ex=6749b790&is=67486610&hm=bf1bcf7bb8902ac53eb1256fc1b40ed212ede023c737eea1083533499146feea&", 
                         title: "View from My Appartment, Toronto", 
                         description: "Date: Nov 10th, 2024",
                         altText: "The Toronto city skyline in the nightime rain with several apartment buildings that have a few units with their lights on, below there is some light trafic on the roads."
                     },
                     { 
-                        url: "https://media.discordapp.net/attachments/1108482530417594461/1306057479498498180/IMG_0466.jpg?ex=67486610&is=67471490&hm=fae784bf35d36854c6a5913fd14b05b3a1b26baf5fb5df6f2cc962aeade14ad9&=&format=webp&width=507&height=676", 
+                        url: "https://cdn.discordapp.com/attachments/1108482530417594461/1306057479498498180/IMG_0466.jpg?ex=6749b790&is=67486610&hm=315658dcf25a6307050a56feea0e3b0c389418ed257fc457dcf1d209447326c4&", 
                         title: "Coffee Cupping event at Subtext Roasters, Toronto", 
                         description: "Date: Nov 9th, 2024",
                         altText: "A table at the forefront with multiple ceramic cups for coffee tasting. A woman in the background is pouring water into the cups from a kettle."
                     },
                     { 
-                        url: "https://media.discordapp.net/attachments/1108482530417594461/1306057480253734974/IMG_0450.jpg?ex=67486610&is=67471490&hm=eed5a686ecb4b3d9e7e33686cc5ff170e77efc59ae600f303e7cd49046b2e543&=&format=webp&width=507&height=676", 
+                        url: "https://cdn.discordapp.com/attachments/1108482530417594461/1306057480253734974/IMG_0450.jpg?ex=6749b790&is=67486610&hm=7d2b70775957400e54f3e32dcf54c8fa714893193946b5b71a827b20e4fbfa4f&", 
                         title: "Homemade Sushi, Toronto", 
                         description: "Date: Nov 6th, 2024",
                         altText: "A white plate with multiple salmon sushi rolls topped with micro green vegetables along with 2 salmon nigiri pieces topped with green onions."
